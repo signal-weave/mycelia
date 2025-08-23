353d11e25129a587ac631d0f503c70000dd5542b
nate-maxwell <natedmaxwell@gmail.com>
2025-08-23 12:58:36 -0700

    comment clarity

--- boot/read.go
+++ boot/read.go
@@ -5,6 +5,7 @@ import "os"
 // -----------------------------------------------------------------------------
 // Herein is the starup process related functions, all neatly placed in one
 // file.
+// This is the top of the cli + pre-init stack.
 // -----------------------------------------------------------------------------
 
 // Parses and stores the runtime flags in public var.
@@ -25,3 +26,5 @@ func ParseRuntimeArgs(argv []string) error {
 
 	return nil
 }
+// ----------
+// ---------------
